f8166e9e12877e6c888c5d848a71e2da73bf2805
Roboguy99 <Roboguy99@gmail.com>
2015-01-23 18:09:23 +0000

    Fixed crash on rendering wrong class

--- src/main/java/roboguy99/itemConverter/proxies/ClientProxy.java
+++ src/main/java/roboguy99/itemConverter/proxies/ClientProxy.java
@@ -3,6 +3,7 @@ package roboguy99.itemConverter.proxies;
 import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 import roboguy99.itemConverter.client.render.block.RenderCrusher;
+import roboguy99.itemConverter.client.render.item.RenderItemCrusher;
 import roboguy99.itemConverter.common.item.Items;
 import roboguy99.itemConverter.common.tile.TileCrusher;
 import cpw.mods.fml.client.registry.ClientRegistry;
@@ -15,6 +16,6 @@ public class ClientProxy extends CommonProxy
 		ClientRegistry.bindTileEntitySpecialRenderer(TileCrusher.class, new RenderCrusher());
 		
 		//Render 3D item models
-		MinecraftForgeClient.registerItemRenderer(Items.itemBlockCrusher, (IItemRenderer)new RenderCrusher());
+		MinecraftForgeClient.registerItemRenderer(Items.itemBlockCrusher, (IItemRenderer)new RenderItemCrusher());
 	}
 }
